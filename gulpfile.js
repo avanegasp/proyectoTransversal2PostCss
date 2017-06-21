d70718e967a91ad78d7382326647ccbff3a56f44
avanegasp <arco527@gmail.com>
2017-06-21 18:35:10 -0500

    Initial proyect

---
+++ gulpfile.js
@@ -0,0 +1,36 @@
+var gulp = require('gulp')
+var postcss = require('gulp-postcss')
+var autoprefixer = require('autoprefixer')
+var browserSync = require('browser-sync').create()
+
+//Servidor de desarrollo
+gulp.task('serve', function(){
+  browserSync.init({
+    server:{
+      baseDir:'./dist'
+    }
+  })
+})
+
+// Tarea para procesar el css
+
+gulp.task('css', function(){
+
+var processors = [
+  autoprefixer({browsers:['>5%', 'ie 8' ]})
+]
+
+  return gulp.src('./src/*.css')
+    .pipe(postcss(processors))
+    .pipe(gulp.dest('./dist/css'))
+    .pipe(browserSync.stream())
+})
+
+// Tarea para vigila los cambios
+
+  gulp.task('watch', function(){
+  gulp.watch('./src/*.css',['css'])
+  gulp.watch('./dist/*.html').on('change', browserSync.reload)
+})
+
+gulp.task('default', ['watch', 'serve'])
